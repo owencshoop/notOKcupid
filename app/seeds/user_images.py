79faa3e5e720f9f9f4167445b5277279d4b9958c
vinceviet <vince.viet@gmail.com>
2023-01-14 11:59:13 -0800

    added seeds

--- app/seeds/user_images.py
+++ app/seeds/user_images.py
@@ -1,15 +1,19 @@
 from app.models import db, UserImage, environment, SCHEMA
 
 
-def see_user_images():
+def seed_user_images():
     user1 = UserImage(
         user_id=1, image_url="https://xsgames.co/randomusers/assets/avatars/male/1.jpg")
     user2 = UserImage(
         user_id=1, image_url="https://xsgames.co/randomusers/assets/avatars/male/2.jpg")
     user3 = UserImage(
         user_id=1, image_url="https://xsgames.co/randomusers/assets/avatars/male/3.jpg")
+    user4 = UserImage(
+        user_id=1, image_url="https://xsgames.co/randomusers/assets/avatars/female/1.jpg")
+    user5 = UserImage(
+        user_id=1, image_url="https://xsgames.co/randomusers/assets/avatars/female/2.jpg")
 
-    user_images = [user1, user2, user3]
+    user_images = [user1, user2, user3, user4, user5]
 
     [db.session.add(image) for image in user_images]
     db.session.commit()
