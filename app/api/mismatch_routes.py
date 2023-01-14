2b6409f97b892e21d50081b1897a487a3b3e7777
vinceviet <vince.viet@gmail.com>
2023-01-14 11:24:43 -0800

    mismatch route

--- app/api/mismatch_routes.py
+++ app/api/mismatch_routes.py
@@ -1,13 +1,14 @@
 from flask import Blueprint
 from app.models import db, Mismatch, User
+from sqlalchemy import or_
 
 mismatch_routes = Blueprint('mismatches', __name__)
 
 
 @mismatch_routes.route('/<int:id>')
 def get_mismatch(id):
-    mismatches = Mismatch.query.filter(
-        Mismatch.user1_id == id or Mismatch.user2_id == id).all()
+    mismatches = Mismatch.query.filter(or_(
+        Mismatch.user1_id == id, Mismatch.user2_id == id)).all()
     if not mismatches:
         return {"errors": ["no matches found"]}, 404
     return [mismatch.to_dict() for mismatch in mismatches], 200
