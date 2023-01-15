91e5be82e9d268e8a0f66a7e8b0a6341864adce3
Nicholas Talbot <talbotnic26@gmail.com>
2023-01-15 16:01:17 -0700

    some cool profile functionality

--- react-app/src/components/ProfilePage/ProfileComponent.js
+++ react-app/src/components/ProfilePage/ProfileComponent.js
@@ -1,13 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { authenticate } from '../../store/session';
+import SingleQuestionForm from '../auth/SingleQuestionForm';
 
 export default function ProfilePage() {
     const user = useSelector(state => state.session.user)
     const [loaded, setLoaded] = useState(false);
     const dispatch = useDispatch()
 
-    console.log(user)
+
 
     useEffect(() => {
         dispatch(authenticate())
@@ -21,6 +22,18 @@ export default function ProfilePage() {
 
 
     return (
-        null
+        <div>
+          <h1>{user.username}'s Profile</h1>
+          <img src={user.userImages[0].imageUrl} />
+          <h3>Name: {user.firstName}</h3>
+          <p>Gender: {user.gender}</p>
+          <p>Preferred Genders: {user.preferredGenders}</p>
+          <p>Age range: {user.minAge} - {user.maxAge}</p>
+
+          <div>
+            <h4>Answer more questions</h4>
+              <SingleQuestionForm />
+          </div>
+        </div>
     )
 }
