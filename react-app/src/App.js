fd942dd0bcbec70fa64edb54e1b2e420fd9e2f18
alexvance9 <alexvance9@gmail.com>
2023-01-15 11:57:27 -0800

    user dislikes tab and cards

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,7 @@ import NavBar from './components/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
+import DislikesLikesComponents from './components/DislikesLikesComponents';
 import { authenticate } from './store/session';
 
 function App() {
@@ -34,6 +35,9 @@ function App() {
         <Route path='/sign-up' exact={true}>
           <SignUpForm />
         </Route>
+        <ProtectedRoute path={['/dislikes', '/likes']}>
+          <DislikesLikesComponents />
+        </ProtectedRoute>
         <ProtectedRoute path='/users' exact={true} >
           <UsersList/>
         </ProtectedRoute>
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -28,6 +28,11 @@ const NavBar = () => {
           </NavLink>
         </li>
         <li>
+          <NavLink to='/dislikes' exact={true} activeClassName='active'>
+            Dislikes
+          </NavLink>
+        </li>
+        <li>
           <LogoutButton />
         </li>
       </ul>
