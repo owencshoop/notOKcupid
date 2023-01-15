8cdb0f12c3feb17a4ecea12bac6bef1db782827e
Nicholas Talbot <talbotnic26@gmail.com>
2023-01-15 10:31:20 -0700

    demo user and questions nav button

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -28,6 +28,11 @@ const NavBar = () => {
           </NavLink>
         </li>
         <li>
+          <NavLink to='/users/:userId/questions' activeClassName='active'>
+            Questions
+          </NavLink>
+        </li>
+        <li>
           <LogoutButton />
         </li>
       </ul>
