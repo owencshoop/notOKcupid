c2dd82d5bb56523bad12d47eb2d8893b4b0649f8
Nicholas Talbot <talbotnic26@gmail.com>
2023-01-15 16:25:50 -0700

    free pics for all

--- react-app/src/components/ProfilePage/ProfileComponent.js
+++ react-app/src/components/ProfilePage/ProfileComponent.js
@@ -24,7 +24,7 @@ export default function ProfilePage() {
     return (
         <div>
           <h1>{user.username}'s Profile</h1>
-          <img src={user.userImages[0].imageUrl} />
+          <img src={user.userImages[0] ? user.userImages[0].imageUrl : 'https://picsum.photos/256/256' } />
           <h3>Name: {user.firstName}</h3>
           <p>Gender: {user.gender}</p>
           <p>Preferred Genders: {user.preferredGenders}</p>
