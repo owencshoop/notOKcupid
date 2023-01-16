0373b060e0827278bd81686830770c2dfabca7b4
vinceviet <vince.viet@gmail.com>
2023-01-16 10:32:22 -0800

    user seed fix

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -49,7 +49,7 @@ def seed_users():
         min_age=18, max_age=100, zip_code=94014, radius=100, bio="This cupid is loved by all and loves all"
     )
     demo5 = User(
-        username="Demo5", email="demo5@aa.io", password="password", age=35, first_name="Demaux", gender="nonbinary", preferred_genders="nonbinary", min_age=26, max_age=36, zip_code=94014, radius=5, bio="I love being a demo user!", dislikes=[cupid, abcde, viktor, micheal]
+        username="Demo5", email="demo5@aa.io", password="password", age=35, first_name="Demaux", gender="nonbinary", preferred_genders="nonbinary", min_age=26, max_age=36, zip_code=94014, radius=5, bio="I love being a demo user!"
     )
     alpal = User(
         username="alpal", email="alpal@aa.io", password="password", age=26, first_name="Alex", gender="female", preferred_genders="male, female, nonbinary", min_age=26, max_age=38, zip_code=94014, radius=2, bio="brb learning to code")
@@ -60,24 +60,35 @@ def seed_users():
     inigo = User(
         username="inigo_montoya", email="inigo_montoya@aa.io", password="password", age=35, first_name="Inigo", gender="male", preferred_genders="female, male, nonbinary", min_age=25, max_age=55, zip_code=94014, radius=7, bio="My name is Inigo Montoya. You killed my father. Prepare to die.")
     brett = User(
-        username='brett', email='brett@aa.io', password='password', age=51, first_name='Brett', gender='male', preferred_genders='female', min_age=30, max_age=80, zip_code=90210, radius=100, bio='I like pickleball and long walks on the beach. #retiredlyf', dislikes=[buttercup, tam], likes=[inigo, optimusa, abcde]
+        username='brett', email='brett@aa.io', password='password', age=51, first_name='Brett', gender='male', preferred_genders='female', min_age=30, max_age=80, zip_code=90210, radius=100, bio='I like pickleball and long walks on the beach. #retiredlyf'
     )
     lyn = User(
-        username='lyn', email='lyn@aa.io', password='password', age=53, first_name='Lyn', gender='female', preferred_genders='male', min_age='30', max_age=85, zip_code=90210, radius=100, bio='Cats are my thing. I foster all types of animals. Ex-field hockey pro', dislikes=[brett, riktor, alpal], likes=[edktor, cupid]
+        username='lyn', email='lyn@aa.io', password='password', age=53, first_name='Lyn', gender='female', preferred_genders='male', min_age='30', max_age=85, zip_code=90210, radius=100, bio='Cats are my thing. I foster all types of animals. Ex-field hockey pro'
     )
     conner = User(
-        username='conner', email='conner@aa.io', password='password', age=28, first_name='Conner', gender='male', preferred_genders='male, female, nonbinary', min_age=18, max_age=99, zip_code=90210, radius=100, bio='Call me dungeon master. Started with world of warcraft and now I run my own games. You can find me in that fresh powpow', dislikes=[viktor, buttercup], likes=[jack, camden]
+        username='conner', email='conner@aa.io', password='password', age=28, first_name='Conner', gender='male', preferred_genders='male, female, nonbinary', min_age=18, max_age=99, zip_code=90210, radius=100, bio='Call me dungeon master. Started with world of warcraft and now I run my own games. You can find me in that fresh powpow'
     )
     eddy = User(
-        username='eddy', email='eddy@aa.io', password='password', age=22, first_name='Eddy', gender='female', preferred_genders='male, female', min_age=18, max_age=50, zip_code=90210, radius=100, bio='I am studying to be a nurse practitioner. I used to play lacrosse but now I hate it with a passion. #cape', dislikes=[tam, alpal, wesley], likes=[edktor, demo5, cupid]
+        username='eddy', email='eddy@aa.io', password='password', age=22, first_name='Eddy', gender='female', preferred_genders='male, female', min_age=18, max_age=50, zip_code=90210, radius=100, bio='I am studying to be a nurse practitioner. I used to play lacrosse but now I hate it with a passion. #cape'
     )
     demo = User(
-        username='demo', email='demo@aa.io', password='password', age=35, first_name='Demo', gender='male', preferred_genders='male', min_age=18, max_age=99, zip_code='90210', radius=100, bio='I will demolish you.', dislikes=[cupid, brett, conner, eddy, riktor, optimusa, micheal, demo5, buttercup, alpal], likes=[camden, wesley, edktor]
+        username='demo', email='demo@aa.io', password='password', age=35, first_name='Demo', gender='male', preferred_genders='male', min_age=18, max_age=99, zip_code='90210', radius=100, bio='I will demolish you.'
     )
 
     users = [demo, brett, lyn, conner, eddy, riktor, viktor, edktor, abcde, optimusa,
              camden, tam, jack, micheal, cupid, demo5, alpal, wesley, buttercup, inigo]
 
+    demo.dislikes = [eddy, conner, brett, cupid]
+    demo.likes = [camden, tam, demo5]
+    demo5.dislikes = [cupid, viktor, abcde, micheal]
+    demo5.likes = [jack, riktor, conner]
+    eddy.dislikes = [demo]
+    conner.dislikes = [demo]
+    brett.dislikes = [demo]
+    cupid.dislikes = [demo, demo5]
+    viktor.dislikes = [demo5]
+    abcde.dislikes = [demo5]
+    micheal.dislikes = [demo5]
     [db.session.add(user) for user in users]
     questions = Question.query.all()
     [[UserAnswer(user=user, question=question)
@@ -91,6 +102,8 @@ def seed_users():
 # incrementing primary key, CASCADE deletes any dependent entities.  With
 # sqlite3 in development you need to instead use DELETE to remove all data and
 # it will reset the primary keys for you as well.
+
+
 def undo_users():
     if environment == "production":
         db.session.execute(
