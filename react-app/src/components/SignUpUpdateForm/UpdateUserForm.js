4a9b466e1b99666d7aab43d21ad32c642d104003
owencshoop <owencshoop@gmail.com>
2023-01-17 16:31:49 -0800

    add signup form styling

--- react-app/src/components/SignUpUpdateForm/UpdateUserForm.js
+++ react-app/src/components/SignUpUpdateForm/UpdateUserForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { updateUser } from "../../store/session";
+import "./SignUpForm";
 
 const UpdateUserForm = () => {
     const user = useSelector((state) => state.session.user);
@@ -11,38 +12,40 @@ const UpdateUserForm = () => {
     const [firstName, setFirstName] = useState(user.firstName);
     const [age, setAge] = useState(user.age);
     const [gender, setGender] = useState(user.gender);
-    const [preferredGenders, setPreferredGenders] = useState(user.preferredGenders);
+    const [preferredGenders, setPreferredGenders] = useState(
+        user.preferredGenders
+    );
     const [minAge, setMinAge] = useState(user.minAge);
     const [maxAge, setMaxAge] = useState(user.maxAge);
     const [city, setCity] = useState(user.city);
     const [state, setState] = useState(user.state);
     const [bio, setBio] = useState(user.bio);
     const [imageURL, setImageURL] = useState(user.userImages[0].imageUrl);
-    const [saved, setSaved] = useState(false)
+    const [saved, setSaved] = useState(false);
     const dispatch = useDispatch();
 
     const onSignUp = async (e) => {
         e.preventDefault();
-            const data = await dispatch(
-                updateUser(
-                    username,
-                    email,
-                    firstName,
-                    age,
-                    gender,
-                    preferredGenders,
-                    minAge,
-                    maxAge,
-                    city,
-                    state,
-                    bio,
-                    imageURL
-                )
-            );
-            if (data) {
-                setErrors(data);
-            }
-        setSaved(true)
+        const data = await dispatch(
+            updateUser(
+                username,
+                email,
+                firstName,
+                age,
+                gender,
+                preferredGenders,
+                minAge,
+                maxAge,
+                city,
+                state,
+                bio,
+                imageURL
+            )
+        );
+        if (data) {
+            setErrors(data);
+        }
+        setSaved(true);
     };
 
     const updateUsername = (e) => {
@@ -98,142 +101,165 @@ const UpdateUserForm = () => {
     }
 
     return (
-        <form onSubmit={onSignUp}>
-            <div>
-                {errors.map((error, ind) => (
-                    <div key={ind}>{error}</div>
-                ))}
+        <div className="user-form-container">
+            <div className="form-input-container">
+                <form onSubmit={onSignUp} className="errors-div-container">
+                    <div>
+                        {errors.map((error, ind) => (
+                            <div className='errors-div' key={ind}>{error}</div>
+                        ))}
+                    </div>
+                    <div className="signup-form-input-container">
+                        <label className="signup-input-label">User Name</label>
+                        <input
+                            type="text"
+                            name="username"
+                            onChange={updateUsername}
+                            value={username}
+                            className="signup-input-field"
+                        ></input>
+                    </div>
+                    <div className="signup-form-input-container">
+                        <label className="signup-input-label">Email</label>
+                        <input
+                            type="text"
+                            name="email"
+                            onChange={updateEmail}
+                            value={email}
+                            className="signup-input-field"
+                        ></input>
+                    </div>
+                    <div className="signup-form-input-container">
+                        <label className="signup-input-label">First Name</label>
+                        <input
+                            type="text"
+                            name="firstName"
+                            onChange={updateFirstName}
+                            value={firstName}
+                            require="true"
+                            className="signup-input-field"
+                        ></input>
+                    </div>
+                    <div className="signup-form-input-container">
+                        <label className="signup-input-label">Age: {age} years</label>
+                        <input
+                            type="range"
+                            name="age"
+                            min="18"
+                            max="100"
+                            onChange={updateAge}
+                            value={age}
+                            className="signup-input-field"
+                        ></input>
+                    </div>
+                    <div className="signup-form-input-container">
+                        <label className="signup-input-label">Gender</label>
+                        <select
+                            name="gender"
+                            onChange={updateGender}
+                            require="true"
+                            className="signup-input-field"
+                        >
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="nonbinary">Nonbinary</option>
+                        </select>
+                    </div>
+                    <div className="signup-form-input-container">
+                        <label className="signup-input-label">Preferred Gender/s</label>
+                        <select
+                            name="preferredGender"
+                            onChange={updatePreferredGenders}
+                            require="true"
+                            className="signup-input-field"
+                        >
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="nonbinary">Nonbinary</option>
+                            <option value="male, female">
+                                Male and Female
+                            </option>
+                            <option value="male, female, nonbinary">
+                                Male, Female, and Nonbinary
+                            </option>
+                            <option value="male, nonbinary">
+                                Male and Nonbinary
+                            </option>
+                            <option value="female, nonbinary">
+                                Female and Nonbinary
+                            </option>
+                        </select>
+                    </div>
+                    <div className="signup-form-input-container">
+                        <label className="signup-input-label">Minimum Age: {minAge} years</label>
+                        <input
+                            type="range"
+                            name="minAge"
+                            min="18"
+                            max="99"
+                            onChange={updateMinAge}
+                            value={minAge}
+                            className="signup-input-field"
+                        ></input>
+                    </div>
+                    <div className="signup-form-input-container">
+                        <label className="signup-input-label">Maximum Age: {maxAge} years</label>
+                        <input
+                            type="range"
+                            name="maxAge"
+                            min={minAge}
+                            max="100"
+                            onChange={updateMaxAge}
+                            value={maxAge}
+                            className="signup-input-field"
+                        ></input>{" "}
+                    </div>
+                    <div className="signup-form-input-container">
+                        <label className="signup-input-label">City</label>
+                        <input
+                            type="text"
+                            name="city"
+                            onChange={updatecity}
+                            value={city}
+                            className="signup-input-field"
+                        ></input>
+                    </div>
+                    <div className="signup-form-input-container">
+                        <label className="signup-input-label">State</label>
+                        <input
+                            type="text"
+                            name="state"
+                            onChange={updateState}
+                            value={state}
+                            className="signup-input-field"
+                        ></input>
+                    </div>
+                    <div className="signup-form-input-container">
+                        <label className="signup-input-label">Bio</label>
+                        <input
+                            type="textarea"
+                            name="bio"
+                            onChange={updateBio}
+                            value={bio}
+                            className="signup-bio-field"
+                        ></input>
+                    </div>
+                    <div className="signup-form-input-container">
+                        <label className="signup-input-label">Profile Image URL</label>
+                        <input
+                            type="text"
+                            name="imageUrl"
+                            onChange={updateImageURL}
+                            value={imageURL}
+                            className="signup-input-field"
+                        ></input>
+                    </div>
+                    <button type="submit" className="sign-up-button">
+                        Save
+                    </button>
+                </form>
             </div>
-            <div>
-                <label>User Name</label>
-                <input
-                    type="text"
-                    name="username"
-                    onChange={updateUsername}
-                    value={username}
-                ></input>
-            </div>
-            <div>
-                <label>Email</label>
-                <input
-                    type="text"
-                    name="email"
-                    onChange={updateEmail}
-                    value={email}
-                ></input>
-            </div>
-            <div>
-                <label>First Name</label>
-                <input
-                    type="text"
-                    name="firstName"
-                    onChange={updateFirstName}
-                    value={firstName}
-                    require="true"
-                ></input>
-            </div>
-            <div>
-                <label>Age</label>
-                <input
-                    type="range"
-                    name="age"
-                    min="18"
-                    max="100"
-                    onChange={updateAge}
-                    value={age}
-                ></input>{" "}
-                {age} years
-            </div>
-            <div>
-                <label>Gender</label>
-                <select name="gender" onChange={updateGender} require="true">
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                    <option value="nonbinary">Nonbinary</option>
-                </select>
-            </div>
-            <div>
-                <label>Preferred Gender/s</label>
-                <select
-                    name="preferredGender"
-                    onChange={updatePreferredGenders}
-                    require="true"
-                >
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                    <option value="nonbinary">Nonbinary</option>
-                    <option value="male, female">Male and Female</option>
-                    <option value="male, female, nonbinary">
-                        Male, Female, and Nonbinary
-                    </option>
-                    <option value="male, nonbinary">Male and Nonbinary</option>
-                    <option value="female, nonbinary">
-                        Female and Nonbinary
-                    </option>
-                </select>
-            </div>
-            <div>
-                <label>Minimum Age</label>
-                <input
-                    type="range"
-                    name="minAge"
-                    min="18"
-                    max="100"
-                    onChange={updateMinAge}
-                    value={minAge}
-                ></input>{" "}
-                {minAge} years
-            </div>
-            <div>
-                <label>Maximum Age</label>
-                <input
-                    type="range"
-                    name="maxAge"
-                    min={minAge}
-                    max="100"
-                    onChange={updateMaxAge}
-                    value={maxAge}
-                ></input>{" "}
-                {maxAge} years
-            </div>
-            <div>
-                <label>City</label>
-                <input
-                    type="text"
-                    name="city"
-                    onChange={updatecity}
-                    value={city}
-                ></input>
-            </div>
-            <div>
-                <label>State</label>
-                <input
-                    type="text"
-                    name="state"
-                    onChange={updateState}
-                    value={state}
-                ></input>
-            </div>
-            <div>
-                <label>Bio</label>
-                <input
-                    type="text"
-                    name="bio"
-                    onChange={updateBio}
-                    value={bio}
-                ></input>
-            </div>
-            <div>
-                <label>Profile Image URL</label>
-                <input
-                    type="text"
-                    name="imageUrl"
-                    onChange={updateImageURL}
-                    value={imageURL}
-                ></input>
-            </div>
-            <button type="submit">Save</button>
-        </form>
+        </div>
     );
 };
 
