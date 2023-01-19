785f41a25f7d7b6f1a090e00dcea801d6d2f086d
owencshoop <owencshoop@gmail.com>
2023-01-18 17:52:20 -0800

    add seed users

--- migrations/versions/20230118_175117_.py
+++ migrations/versions/20230118_175117_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 60494b74892b
+Revision ID: a74cdc9d5cee
 Revises: 
-Create Date: 2023-01-18 11:00:09.005055
+Create Date: 2023-01-18 17:51:17.989426
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '60494b74892b'
+revision = 'a74cdc9d5cee'
 down_revision = None
 branch_labels = None
 depends_on = None
