cb19fc63a0e11256324d03f20966392b4d4b46e2
Nicholas Talbot <talbotnic26@gmail.com>
2023-01-18 18:51:20 -0700

    some content on the footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,7 +1,31 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import './Footer.css'
 
 export default function Footer () {
-  
+
+
+
+
+  return (
+    <>
+    <div className="footer-container">
+        <div className="technology-container">
+          <span>Technologies: </span>
+            <span>Flask</span>
+            <span>React</span>
+            <span>Redux</span>
+            <span>PostgreSQL</span>
+        </div>
+        <div className="github-container">
+          <span className="git-links">Creator Github Links: </span>
+            <a href='https://github.com/owencshoop' className="git-links">Owen Shoop</a>
+            <a href='https://github.com/alexvance9' className="git-links">Alex Vance</a>
+            <a href='https://github.com/vinceviet' className="git-links">Vince Viet</a>
+            <a href='https://github.com/nicisherenow' className="git-links">Nicholas Talbot</a>
+        </div>
+    </div>
+    </>
+  )
 }
