11aa5457db897ed5f24f691aff713660508fadb9
vinceviet <vince.viet@gmail.com>
2023-01-19 11:13:34 -0800

    percentage added

--- react-app/src/components/DiscoverPage/DiscoverPage.js
+++ react-app/src/components/DiscoverPage/DiscoverPage.js
@@ -30,32 +30,32 @@ export default function Discover() {
     if (!users) {
         return (
             <>
-            <div className="discover-header-container">
-                <h1>
-                Discover
-                </h1>
-            </div>
-            <div className="discover-placeholder-div"></div>;
+                <div className="discover-header-container">
+                    <h1>
+                        Discover
+                    </h1>
+                </div>
+                <div className="discover-placeholder-div"></div>;
             </>
-            )
+        )
     }
 
     if (usersList.length === 0) {
         return (
             <>
-            <div className="discover-header-container">
-            <h1>
-            Discover
-            </h1>
-        </div>
-        <div className="discover-page-placeholder">
+                <div className="discover-header-container">
+                    <h1>
+                        Discover
+                    </h1>
+                </div>
+                <div className="discover-page-placeholder">
 
-            <h2 >
-                No more users match your preferences, adjust preferences to see
-                more.
-            </h2>
-            <img src={sadPanda} alt='alone-for-now' />
-        </div>
+                    <h2 >
+                        No more users match your preferences, adjust preferences to see
+                        more.
+                    </h2>
+                    <img src={sadPanda} alt='alone-for-now' />
+                </div>
             </>
         );
     }
@@ -68,9 +68,9 @@ export default function Discover() {
     let differentAnswerCount = 0.0
     user1answers.forEach(user1answer => {
         user2answers.forEach(user2answer => {
-            if (user1answer.questionId === user2answer.questionId){
+            if (user1answer.questionId === user2answer.questionId) {
                 sameQuestionCount += 1
-                if (user1answer.answer !== user2answer.answer){
+                if (user1answer.answer !== user2answer.answer) {
                     differentAnswerCount += 1
                 }
             }
@@ -115,79 +115,79 @@ export default function Discover() {
     if (!loaded) {
         return (
             <>
-            <div className="discover-header-container">
-                    
-                <h1>
-                Discover
-                </h1>
-            </div>
-            <div className="discover-placeholder-div"></div>;
+                <div className="discover-header-container">
+
+                    <h1>
+                        Discover
+                    </h1>
+                </div>
+                <div className="discover-placeholder-div"></div>;
             </>
-            )
+        )
     }
     if (!user) {
         return (
             <>
-            <div className="discover-header-container">
-                <h1>
-                Discover
-                </h1>
-            </div>
-            <div className="discover-placeholder-div"></div>;
+                <div className="discover-header-container">
+                    <h1>
+                        Discover
+                    </h1>
+                </div>
+                <div className="discover-placeholder-div"></div>;
             </>
-            )
+        )
     }
 
     return (
         <>
-        <div className="discover-header-container">
-               <div className="pref-button-container">
-                <img id='equalizer' alt='pref-slider' src={slider} />
-                <OpenModalButton
-                    buttonText="Update Preferences"
-                    modalComponent={<UpdatePreferencesForm />}
-                className="preferences-open-button"
-                />
-               </div>
-            <h1>
-            Discover
-            </h1>
-        </div>
-        <div className='discover-page-containter'>
-            <ul>
-                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-            </ul>
-            <div className='discover-page-header'>
-                <div className='discover-page-top'>
-                    <div className='discover-name-age'>
-                    <span>{user.firstName[0].toUpperCase() + user.firstName.slice(1)}{" "}  • {user.age}</span>
-                    <span>{user.city}, {user.state}</span>
+            <div className="discover-header-container">
+                <div className="pref-button-container">
+                    <img id='equalizer' alt='pref-slider' src={slider} />
+                    <OpenModalButton
+                        buttonText="Update Preferences"
+                        modalComponent={<UpdatePreferencesForm />}
+                        className="preferences-open-button"
+                    />
+                </div>
+                <h1>
+                    Discover
+                </h1>
+            </div>
+            <div className='discover-page-containter'>
+                <ul>
+                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                </ul>
+                <div className='discover-page-header'>
+                    <div className='discover-page-top'>
+                        <div className='discover-name-age'>
+                            <span id='user-name'>{user.firstName[0].toUpperCase() + user.firstName.slice(1)}{" "}  • {user.age}</span>
+                            <span>{user.city}, {user.state}</span>
+                        </div>
+                        <div className='buttons-container'>
+                            {!isNaN(mismatchPercentage) && <span className='percentage'>{mismatchPercentage}%</span>}
+                            <button className="dislike-button" onMouseUp={handleDislike}><img id='discover-button-icons' src={heart} />Dislike</button>
+                            <button className="like-button" onMouseUp={handleLike}><img id='discover-button-icons' src={thumb} />Like</button>
+                            <button onMouseUp={updateUserNumber} className='skip-button'>Skip</button>
+                        </div>
                     </div>
-                    <div className='buttons-container'>
-                        {!isNaN(mismatchPercentage) && <div>{mismatchPercentage}</div>}
-                        <button className="dislike-button" onMouseUp={handleDislike}><img id='discover-button-icons' src={heart} />Dislike</button>
-                        <button className="like-button" onMouseUp={handleLike}><img id='discover-button-icons' src={thumb} />Like</button>
-                        <button onMouseUp={updateUserNumber} className='skip-button'>Skip</button>
+                    <img id='discover-image'
+                        alt="discover-pic"
+                        src={
+                            user.userImages[0]
+                                ? user.userImages[0].imageUrl
+                                : "https://picsum.photos/256/256"
+                        }
+                    />
+                </div>
+                <div className='discover-info'>
+                    <h3 className='info-header'>About {user.firstName}</h3>
+                    <div className='info'>
+                        <span>Gender: {user.gender}</span>
+                        <span>Preferred Genders: {user.preferredGenders}</span>
+                        <span>Bio: {user.bio}</span>
                     </div>
                 </div>
-                <img id='discover-image'
-                    alt="discover-pic"
-                    src={
-                        user.userImages[0]
-                            ? user.userImages[0].imageUrl
-                            : "https://picsum.photos/256/256"
-                        }
-                        />
             </div>
-            <div className='discover-info'>
-            <h3 className='info-header'>About {user.firstName}</h3>
-            <div className='info'>
-                <span>Gender: {user.gender}</span>
-                <span>Preferred Genders: {user.preferredGenders}</span>
-                <span>Bio: {user.bio}</span>
-            </div>
-        </div>
-        </div>
-                        </>
+        </>
     );
 }
